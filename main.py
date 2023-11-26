7ac60549ae42938331c202e098f044e85b9ed4d3
AnnaOvsSheep <x180475@yandex.ru>
2023-11-26 20:46:50 +0300

    Add sub function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from funcs import sum_num
+from funcs import sum_num, sub_num
 
 
 def main():
@@ -6,6 +6,8 @@ def main():
     b = int(input())
     sum_ = sum_num(a, b)
     print(sum_)
+    sub_ = sub_num(a, b)
+    print(sub_)
 
 if __name__ == "__main__":
     main()
